ae26b8369499eaa04f461397585230f0eeda8483
bebyakovdaniil <bebyakov2014@gmail.com>
2026-02-09 16:58:41 +0300

    tgbot+bd

---
+++ tgbot_test/Host.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace tgbot_test
+{
+
+    public class Host
+    {
+        public Action<ITelegramBotClient, Update>? OnMessage;
+        private TelegramBotClient _bot;
+        public Host(string token)
+        {
+            _bot=new TelegramBotClient(token);
+        }
+        public void Start()
+        {
+            _bot.StartReceiving(UpdateHandler, ErrorHandler);
+            Console.WriteLine("Бот запущен");
+        }
+
+        private async Task ErrorHandler(ITelegramBotClient client, Exception exception, HandleErrorSource source, CancellationToken token)
+        {
+            Console.WriteLine("Ошибка: "+exception.Message);
+            await Task.CompletedTask;
+        }
+
+        private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            Console.WriteLine($"Пришло сообщение: {update.Message?.Text ?? "не текст"}");
+            OnMessage?.Invoke(client, update);
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ tgbot_test/Program.cs
@@ -0,0 +1,27 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using tgbot_test;
+using Npgsql;
+using GymBot.Data;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Host gymbot = new Host("8403810096:AAE5YTeO8IYbxWFKC_v79WyiqvqvfArz3lM");
+        gymbot.Start();
+        gymbot.OnMessage += OnMessage;
+        Console.ReadLine(); 
+    }
+    string connection = "Host=localhost;Port=5432;Username=gymbotuser;Password=gymbotpass;Database=gymbot_db";
+
+    static UserRepository Users = new UserRepository();
+    private static async void OnMessage(ITelegramBotClient client, Update update)
+    {
+        if (update.Message == null) return;
+        await Users.AddUser(update.Message.Chat.Id, update.Message.From?.Username);
+        await client.SendMessage(update.Message.Chat.Id, "Теперь ты в файлах Эйпштена, пидар.");
+        await client.SendMessage(update.Message?.Chat.Id ?? 445584914, update.Message?.Text?? "[не текст]");
+    }
+}
