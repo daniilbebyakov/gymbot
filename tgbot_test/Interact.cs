3d5cad6f3cb7e540e07852064124e13d3fd375ee
bebyakovdaniil <bebyakov2014@gmail.com>
2026-02-11 13:48:00 +0300

    fix token + onmessage

---
+++ tgbot_test/Interact.cs
@@ -0,0 +1,35 @@
+﻿using GymBot.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace tgbot_test
+{
+    internal class Interact
+    {
+        static UserRepository Users = new UserRepository();
+        public static async void OnMessage(ITelegramBotClient client, Update update)
+        {
+            if (update.Message == null) return;
+            string usermessage = update.Message.Text ?? "";
+            switch (usermessage)
+            {
+                case "/start":
+                    bool added = await Users.AddUserIfNotExist(update.Message.Chat.Id, update.Message.From?.Username ?? "");
+                    if (added)
+                    {
+                        await client.SendMessage(update.Message.Chat.Id, "Теперь ты в файлах Эйпштена, пидар.");
+                    }
+                    break;
+                case "/me":
+                    await client.SendMessage(update.Message.Chat.Id, $"Твой id: {update.Message.Chat.Id}\nТвой ник: {update.Message.From?.Username ?? "Нет ника"}");
+                    break;
+                default:
+                    await client.SendMessage(update.Message?.Chat.Id ?? 445584914, update.Message?.Text ?? "[не текст]");
+                    break;
+            }
+        }
+    }
+}
--- tgbot_test/Program.cs
+++ tgbot_test/Program.cs
@@ -9,32 +9,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Host gymbot = new Host("8403810096:AAE5YTeO8IYbxWFKC_v79WyiqvqvfArz3lM");
+        Host gymbot = new Host(Hidden.token);
         gymbot.Start();
-        gymbot.OnMessage += OnMessage;
+        gymbot.OnMessage += Interact.OnMessage;
         Console.ReadLine(); 
-    }
-
-    static UserRepository Users = new UserRepository();
-    private static async void OnMessage(ITelegramBotClient client, Update update)
-    {
-        if (update.Message == null) return;
-        string usermessage = update.Message.Text ?? "";
-        switch (usermessage)
-        {
-            case "/start":
-                bool added = await Users.AddUserIfNotExist(update.Message.Chat.Id, update.Message.From?.Username ?? "");
-                if (added)
-                {
-                    await client.SendMessage(update.Message.Chat.Id, "Теперь ты в файлах Эйпштена, пидар.");
-                }
-                break;
-            case "/me":
-                await client.SendMessage(update.Message.Chat.Id, $"Твой id: {update.Message.Chat.Id}\nТвой ник: {update.Message.From?.Username ?? "Нет ника"}");
-                break;
-            default:
-                await client.SendMessage(update.Message?.Chat.Id ?? 445584914, update.Message?.Text ?? "[не текст]");
-                break;
-        } 
-    }
+    } 
 }
