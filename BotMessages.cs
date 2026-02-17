14e6d505760f87bc4c7cbf4db0128fc357449417
daniilbebyakov <bebyakov2014@gmail.com>
2026-02-17 15:41:00 +0300

    upd

--- BotMessages.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public static class BotMessages
-{
-     public const string BotStarted = "Бот запущен";
-    public const string BotError = "Ошибка";
-    public const string BotNewMessage = "Пришло сообщение";
-}
